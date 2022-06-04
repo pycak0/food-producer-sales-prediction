5ddf6663302aadb929673c8ba4402e6dfa4fbbc7
Владислав Русаков <vladisrusakov@yandex.ru>
2022-06-04 22:28:30 +0300

    Refactor utils to meet pep8 requirements

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
-# Imports
+"""Файл с общими полезными методами.
+"""
 
+from typing import Iterable
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,8 +9,20 @@ import matplotlib.pyplot as plt
 
 # Data Manipulation
 
-# метод для агрегирования данных по неделям/месяцам и т.д.
-def group_data(data: pd.DataFrame, date_interval: str):
+def group_data(data: pd.DataFrame, date_interval: str) -> pd.DataFrame:
+    """Агрегировать данные по неделям/месяцам и т.д.
+
+    Args:
+        data (pd.DataFrame): Данные для агрегации, должны содержать столбцы
+            'DFU', 'Customer', 'Period', 'BPV' и 'Total Sell-in'.
+        date_interval (str): Интервал агрегации.
+            Допустимые значения см. в разделе 'Offset aliases' документации pandas.
+
+    Returns:
+        pd.DataFrame: Датафрейм со значениями BPV и Total Sell-in,
+            агрегированными по заданному интервалу.
+    """
+
     return data.groupby(
         ['DFU', 'Customer', data['Period'].dt.to_period(date_interval)]
     ).agg(
@@ -16,17 +30,19 @@ def group_data(data: pd.DataFrame, date_interval: str):
     ).reset_index()
 
 
-# метод для удаления выбросов
-def clean_data(data: pd.DataFrame, replace_with: str = '3sigma', comments: bool = False):
-    '''
-    replace_with: 'mean' / '3sigma'
-    '''
+def clean_data(data: pd.DataFrame, replace_with: str = '3sigma') -> pd.DataFrame:
+    """Удалить выбросы в данных.
+
+    Args:
+        data (pd.DataFrame): Исходные данные.
+        replace_with (str, optional): 'mean' или '3sigma'. По умолчанию '3sigma'.
+
+    Returns:
+        pd.DataFrame: Копия исходных данных с обработкой выбросов.
+    """
+
     sigma = data['BPV'].std()
     mean = data['BPV'].mean()
-
-    if comments:
-        print(f"STD: {sigma}; 3 * std: {3 * sigma}")
-        print(f'Mean: {mean}')
 
     data_cleaned = data.copy()
     replacement = mean if replace_with == 'mean' else 3 * sigma
@@ -34,8 +50,17 @@ def clean_data(data: pd.DataFrame, replace_with: str = '3sigma', comments: bool 
     return data_cleaned
 
 
-# метод для создания новых признаков (день, месяц, год, квартал)
 def make_date_features(X: pd.DataFrame, date_col_name='Period') -> pd.DataFrame:
+    """Создать новые признаки (день, месяц, год, квартал).
+
+    Args:
+        X (pd.DataFrame): Исходный датафрейм.
+        date_col_name (str, optional): Название столбца с датой в X. По умолчанию 'Period'.
+
+    Returns:
+        pd.DataFrame: Копия переданного датафрейма с добавленными признаками.
+    """
+
     X_new = X.copy()
     date_info = X[date_col_name].dt
     X_new['year'] = date_info.year
@@ -49,39 +74,77 @@ def make_date_features(X: pd.DataFrame, date_col_name='Period') -> pd.DataFrame:
 
 # Metrics
 
-# подсчет метрики wape
-def wape_metric(actual: np.array, predicted: np.array):
+def wape_metric(actual: Iterable[float], predicted: Iterable[float]) -> float:
+    """Подсчитать метрику WAPE.
+
+    Args:
+        actual (Iterable[float]): Последовательность реальных значений целевой переменной.
+        predicted (Iterable[float]): Последовательность предсказанных значений.
+
+    Returns:
+        float: Подсчитанная метрика WAPE.
+    """
+
     return np.abs(np.array(actual) - np.array(predicted)).sum() / np.array(actual).sum()
 
 
-# подсчет метрики 1-wape
-def quality(actual: np.array, predicted: np.array):
+def quality(actual: np.array, predicted: np.array) -> float:
+    """Подсчитать метрику (1 - WAPE).
+
+    Args:
+        actual (np.array): Последовательность реальных значений целевой переменной.
+        predicted (np.array): Последовательность предсказанных значений.
+
+    Returns:
+        float: Подсчитанная метрика (1 - WAPE).
+    """
+
     return 1 - wape_metric(actual, predicted)
 
 
-# метод для сохранения прогнозов в csv файл
 def save_results(actual: np.array, predicted: np.array, comment=None):
+    """Сохранить прогнозы модели и реальные значения целевой переменной в формате .csv.
+
+    Args:
+        actual (np.array): Последовательность реальных значений целевой переменной.
+        predicted (np.array): Последовательность предсказанных значений.
+        comment ([type], optional): Префикс для названия файла. По умолчанию None (не используется).
+    """
+
     prefix = '' if comment is None else f'{comment}-'
     filename = f'{prefix}results.csv'
-    
+
     pd.DataFrame({
         'actual': actual,
         'predicted': predicted
     }).to_csv(filename, index=False)
 
 
-# метод для визулизации прогнозов
-def plot_forecast(series_train, series_test, forecast, forecast_int=None):
+def plot_forecast(
+    series_train: pd.Series,
+    series_test: pd.Series,
+    forecast: pd.Series,
+    forecast_int=None
+):
+    """Визуализировать прогноз модели.
+
+    Args:
+        series_train (pd.Series): Тренировочные данные BPV.
+        series_test (pd.Series): Тестовые данные BPV.
+        forecast (pd.Series): Предсказанные данные BPV.
+        forecast_int ([type], optional): Доверительный интервал. По умолчанию None.
+    """
 
     wape = wape_metric(series_test, forecast)
-    quality = 1 - wape
+    qual = 1 - wape
 
     plt.figure(figsize=(12, 6))
-    plt.title(f"WAPE: {wape:.2f}  Quality: {quality:.2f}")
+    plt.title(f"WAPE: {wape:.2f}  Quality: {qual:.2f}")
+
     series_train.plot(label="train", color="b")
     series_test.plot(label="test", color="g")
-    # forecast.index = series_test.index
     forecast.plot(label="forecast", color="r")
+
     if forecast_int is not None:
         plt.fill_between(
             series_test.index,
@@ -90,6 +153,7 @@ def plot_forecast(series_train, series_test, forecast, forecast_int=None):
             alpha=0.2,
             color="dimgray",
         )
+
     plt.legend(prop={"size": 16})
     plt.show()
 
@@ -97,19 +161,47 @@ def plot_forecast(series_train, series_test, forecast, forecast_int=None):
 # Repository
 
 class Repo:
+    """Класс-обертка над DataFrame для удобного выбора нужных данных.
+
+    init args:
+        df (pd.DataFrame): Исходные данные со столбцами
+            'Period', 'DFU' и 'Customer'.
+    """
 
     def __init__(self, df: pd.DataFrame):
         self.df = df
         if 'Unnamed: 0' in self.df.columns:
             self.df.drop(columns=['Unnamed: 0'], inplace=True)
 
-    def get_data(self, cust: int = None, dfu: str = None, date_from: str = None, date_to: str = None, date_interval: str = None):
+    def get_data(
+        self,
+        cust: int = None,
+        dfu: str = None,
+        date_from: str = None,
+        date_to: str = None,
+        date_interval: str = None
+    ) -> pd.DataFrame:
+        """Получить данные по заданным параметрам.
+        Если параметр равен None, то фильтрация по нему не производится.
+
+        Args:
+            cust (int, optional): Идентификатор клиента. По умолчанию None.
+            dfu (str, optional): DFU - название продукта. По умолчанию None.
+            date_from (str, optional): С какой даты выбрать данные. По умолчанию None.
+            date_to (str, optional): По какую дату выбрать данные. По умолчанию None.
+            date_interval (str, optional): Интервал для агрегации. По умолчанию None.
+                Допустимые значения см. в методе group_data.
+
+        Returns:
+            pd.DataFrame: Результат выбора данных по заданным параметрам.
+        """
+
         data = self.df
-        
+
         if cust is not None:
             data = data[data['Customer'] == cust]
 
-        if dfu is not None: 
+        if dfu is not None:
             data = data[data['DFU'] == dfu]
 
         if 'Period' in data.columns:
@@ -127,11 +219,20 @@ class Repo:
         return data
 
     def show_data_for(self, cust: int, dfu: str, save_plot: bool = False):
+        """Выбрать из данных пару [DFU x Customer] и построить для них график.
+
+        Args:
+            cust (int): Идентификатор клиента.
+            dfu (str): DFU - название продукта.
+            save_plot (bool, optional): Сохранить изображение графика или нет.
+                По умолчанию False (не сохранять).
+        """
+
         data = self.get_data(cust, dfu)
         data_cleaned = clean_data(data)
         max_bpv = data['BPV'].max() + 10
 
-        fig, axs = plt.subplots(2, 1, figsize=(21, 16))
+        _, axs = plt.subplots(2, 1, figsize=(21, 16))
         top, bottom = axs[0], axs[1]
 
         top.plot(
